1646cac4e0781924d005a283531ba08084e2615f
misfarsiddeek95 <asmmisfar@gmail.com>
2025-06-25 03:43:13 +0530

    permission changes and logo changes

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -2,8 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta name="description" content="">
-<title>Fancy Point | Back office</title>
-<link rel="icon" type="image/png" href="<?=base_url()?>assets/img/favicon.png">
+<title>Make-Kit | Control Panel</title>
+<link rel="icon" type="image/png" href="<?=base_url()?>assets/img/favicon_io/favicon-32x32.png">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
 <link rel="stylesheet" href="<?=base_url()?>assets/css/vendor.css">
 <link rel="stylesheet" href="<?=base_url()?>assets/css/cosmos.css">
--- application/views/includes/topbar.php
+++ application/views/includes/topbar.php
@@ -1,335 +1,77 @@
 <div class="site-overlay"></div>
 
-  <div class="site-header">
+<div class="site-header">
+  <nav class="navbar navbar-default">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?= base_url() ?>" style="padding: 1px 10px;">
+        <img src="<?= base_url() ?>assets/img/logo.png" alt="Logo" height="45">
+      </a>
 
-    <nav class="navbar navbar-default">
+      <!-- Sidebar toggles -->
+      <button class="navbar-toggler left-sidebar-toggle pull-left visible-xs" type="button">
+        <span class="hamburger"></span>
+      </button>
 
-      <div class="navbar-header">
+      <button class="navbar-toggler pull-right visible-xs-block" type="button" data-toggle="collapse" data-target="#navbar">
+        <span class="more"></span>
+      </button>
+    </div>
 
-        <a class="navbar-brand" href="<?=base_url()?>" style="padding: 1px 10px;">
+    <div class="navbar-collapsible">
+      <div id="navbar" class="navbar-collapse collapse">
 
-          <img src="<?=base_url()?>assets/img/logo.gif" alt="" height="45">
-
-        </a>
-
-        <button class="navbar-toggler left-sidebar-toggle pull-left visible-xs" type="button">
-
+        <!-- Desktop toggle -->
+        <button class="navbar-toggler left-sidebar-collapse pull-left hidden-xs" type="button">
           <span class="hamburger"></span>
-
         </button>
 
-        <!-- <button class="navbar-toggler right-sidebar-toggle pull-right visible-xs-block" type="button">
-
-          <i class="zmdi zmdi-long-arrow-left"></i>
-
-          <div class="dot bg-danger"></div>
-
-        </button> -->
-
-        <button class="navbar-toggler pull-right visible-xs-block" type="button" data-toggle="collapse" data-target="#navbar">
-
-          <span class="more"></span>
-
-        </button>
-
-      </div>
-
-      <div class="navbar-collapsible">
-
-        <div id="navbar" class="navbar-collapse collapse">
-
-          <button class="navbar-toggler left-sidebar-collapse pull-left hidden-xs" type="button">
-
-            <span class="hamburger"></span>
-
-          </button>
-
-          <!-- <button class="navbar-toggler right-sidebar-toggle pull-right hidden-xs" type="button">
-
-            <i class="zmdi zmdi-long-arrow-left"></i>
-
-            <div class="dot bg-danger"></div>
-
-          </button> -->
-
-          <ul class="nav navbar-nav">
-
-            <li class="visible-xs-block">
-
-              <div class="nav-avatar">
-
-                <img class="img-circle" src="<?=base_url()?>photos/<?=$this->session->userdata['staff_logged_in']['image']?>" width="48" height="48">
-
-              </div>
-
-              <h4 class="navbar-text text-center">Welcome, <?=$this->session->userdata['staff_logged_in']['name']?>!</h4>
-
-            </li>
-
-          </ul>
-
-         <!--  <form class="navbar-form navbar-left">
-
-            <div class="navbar-search-group">
-
-              <input type="text" class="form-control" placeholder="Search">
-
-              <button type="submit" class="btn btn-default">
-
-                <i class="zmdi zmdi-search"></i>
-
-              </button>
-
+        <ul class="nav navbar-nav">
+          <li class="visible-xs-block">
+            <div class="nav-avatar">
+              <img class="img-circle" src="<?= base_url() ?>photos/<?= $this->session->userdata['staff_logged_in']['image'] ?>" width="48" height="48">
             </div>
-
-          </form> -->
-
-          <ul class="nav navbar-nav navbar-right">
-
-            <li class="nav-table dropdown visible-xs-block">
-
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-
-                <span class="nav-cell nav-icon">
-
-                  <i class="zmdi zmdi-account-o"></i>
-
-                </span>
-
-                <span class="hidden-md-up m-l-15">Account</span>
-
-              </a>
-
-              <ul class="dropdown-menu">
-
-                <li>
-
-                  <a href="<?=base_url('profile')?>">
-
-                    <i class="zmdi zmdi-account-o m-r-10"></i> Profile</a>
-
-                </li>
-
-                <li role="separator" class="divider"></li>
-
-                <li><a href="<?= base_url('logout')?>">Logout</a></li>
-
-              </ul>
-
-            </li>
-
-            <!-- <li class="nav-table dropdown">
-
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-
-                <span class="nav-cell nav-icon">
-
-                  <i class="zmdi zmdi-notifications-none"></i>
-
-                </span>
-
-                <span class="hidden-md-up m-l-15">Notifications</span>
-
-                <span class="label label-warning">7</span>
-
-              </a>
-
-              <div class="dropdown-menu custom-dropdown dropdown-messages dropdown-menu-right">
-
-                <div class="dropdown-header">
-
-                  <span>Notifications</span>
-
-                  <a href="#" class="text-primary">Mark all as read</a>
-
-                </div>
-
-                <div class="m-items">
-
-                  <div class="custom-scrollbar">
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-warning">
-
-                          <i class="zmdi zmdi-upload"></i>
-
-                        </div>
-
-                        <div class="mi-time">10 min</div>
-
-                        <div class="mi-title">Upload status</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-success">
-
-                          <i class="zmdi zmdi-money"></i>
-
-                        </div>
-
-                        <div class="mi-time">40 min</div>
-
-                        <div class="mi-title">Income</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-primary">
-
-                          <i class="zmdi zmdi-alert-triangle"></i>
-
-                        </div>
-
-                        <div class="mi-time">3 hours</div>
-
-                        <div class="mi-title">New task</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-warning">
-
-                          <i class="zmdi zmdi-upload"></i>
-
-                        </div>
-
-                        <div class="mi-time">10 min</div>
-
-                        <div class="mi-title">Upload status</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-success">
-
-                          <i class="zmdi zmdi-money"></i>
-
-                        </div>
-
-                        <div class="mi-time">40 min</div>
-
-                        <div class="mi-title">Income</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                    <div class="m-item">
-
-                      <a href="#">
-
-                        <div class="mi-icon bg-primary">
-
-                          <i class="zmdi zmdi-alert-triangle"></i>
-
-                        </div>
-
-                        <div class="mi-time">3 hours</div>
-
-                        <div class="mi-title">New task</div>
-
-                        <div class="mi-text text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-
-                      </a>
-
-                    </div>
-
-                  </div>
-
-                </div>
-
-                <div class="dropdown-footer">
-
-                  <a href="#">View all notifications</a>
-
-                </div>
-
-              </div>
-
-            </li> -->
-
-            <li class="nav-table dropdown hidden-sm-down">
-
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-
-                <span class="nav-cell p-r-10">
-
-                  <img class="img-circle" src="<?=base_url()?>photos/<?=$this->session->userdata['staff_logged_in']['image']?>" alt="" width="32" height="32">
-
-                </span>
-
-                <span class="nav-cell"><?=$this->session->userdata['staff_logged_in']['name']?>
-
-                  <span class="caret"></span>
-
-                </span>
-
-              </a>
-
-              <ul class="dropdown-menu">
-
-                <li>
-
-                    <a href="<?=base_url('profile')?>">
-
-                      <i class="zmdi zmdi-account-o m-r-10"></i> Profile</a>
-
-                  </li>
-
-                  <li role="separator" class="divider"></li>
-
-                  <li>
-
-                    <a href="<?=base_url('logout')?>">
-
-                      <i class="zmdi zmdi-power m-r-10"></i> Logout</a>
-
-                  </li>
-
-              </ul>
-
-            </li>
-
-          </ul>
-
-        </div>
+            <h4 class="navbar-text text-center">Welcome, <?= $this->session->userdata['staff_logged_in']['name'] ?>!</h4>
+          </li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+
+          <!-- Account for mobile view -->
+          <li class="nav-table dropdown visible-xs-block">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <span class="nav-cell nav-icon">
+                <i class="zmdi zmdi-account-o"></i>
+              </span>
+              <span class="hidden-md-up m-l-15">Account</span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="<?= base_url('profile') ?>"><i class="zmdi zmdi-account-o m-r-10"></i> Profile</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="<?= base_url('logout') ?>"><i class="zmdi zmdi-power m-r-10"></i> Logout</a></li>
+            </ul>
+          </li>
+
+          <!-- Profile for desktop view -->
+          <li class="nav-table dropdown hidden-sm-down">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <span class="nav-cell p-r-10">
+                <img class="img-circle" src="<?= base_url() ?>photos/<?= $this->session->userdata['staff_logged_in']['image'] ?>" alt="" width="32" height="32">
+              </span>
+              <span class="nav-cell">
+                <?= $this->session->userdata['staff_logged_in']['name'] ?>
+                <span class="caret"></span>
+              </span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="<?= base_url('profile') ?>"><i class="zmdi zmdi-account-o m-r-10"></i> Profile</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="<?= base_url('logout') ?>"><i class="zmdi zmdi-power m-r-10"></i> Logout</a></li>
+            </ul>
+          </li>
+
+        </ul>
 
       </div>
-
-    </nav>
-
-  </div>
+    </div>
+  </nav>
+</div>
