050fffe7509ee2143e43b6965be14c337e73a230
misfarsiddeek95 <asmmisfar@gmail.com>
2025-06-28 14:46:29 +0530

    border radius added for the dashboard widgets

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,6 +2,11 @@
 <html lang="en">
   <head>
     <?php $this->load->view('includes/head'); ?>
+    <style>
+      .br-8 {
+        border-radius: 8px;
+      }
+    </style>
   </head>
   <body class="layout layout-header-fixed layout-left-sidebar-fixed">
     <?php $this->load->view('includes/topbar'); ?>
@@ -11,7 +16,7 @@
       <div class="site-content">
         <div class="row">
           <div class="col-md-4 col-sm-4">
-            <div class="widget widget-tile-2 bg-info m-b-30">
+            <div class="widget widget-tile-2 bg-info m-b-30 br-8">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Total Success Orders</div>
                 <div class="wt-number"><?=$totalorders?></div>
@@ -26,7 +31,7 @@
             </div>
           </div>
           <div class="col-md-4 col-sm-4">
-            <div class="widget widget-tile-2 bg-warning m-b-30">
+            <div class="widget widget-tile-2 bg-warning m-b-30 br-8">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Total Product(s)</div>
                 <div class="wt-number"><?=$totalprolist?></div>
@@ -41,7 +46,7 @@
             </div>
           </div>          
           <div class="col-md-4 col-sm-4">
-            <div class="widget widget-tile-2 bg-primary m-b-30">
+            <div class="widget widget-tile-2 bg-primary m-b-30 br-8">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Total user(s)
                   <span class="t-caret text-success">
@@ -63,7 +68,7 @@
         <div class="row">
           <a href="<?=base_url()?>/products/view_Outproducts/9">
           <div class="col-md-4 col-sm-4">
-            <div class="widget widget-tile-2 bg-danger m-b-30">
+            <div class="widget widget-tile-2 bg-danger m-b-30 br-8">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Out of Stocks</div>
                 <div class="wt-number"><?=$outOfStocks?></div>
