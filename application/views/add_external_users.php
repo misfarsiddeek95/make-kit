5fca297befa7b059bb2c0e9f1675d3e5f4e5587f
misfarsiddeek95 <asmmisfar@gmail.com>
2025-06-29 05:50:00 +0530

    completed student form design.

--- application/views/add_external_users.php
+++ application/views/add_external_users.php
@@ -20,20 +20,20 @@
                                 <form data-toggle="validator" id="inputmasks">
                                 <input type="hidden" name="user_id" id="user_id" value="<?php if(!(empty($user))){echo($user->user_id);}else{echo(0);} ?>" />
                                 <div class="row">
-                                    <dic class="col-sm-6 col-md-6">
+                                    <div class="col-sm-6 col-md-6">
                                         <div class="form-group">
                                             <label for="name" class="control-label">Name</label>
                                             <input type="text" pattern="^[a-zA-Z. ]+$" value="<?php if(!(empty($user))){echo($user->name);} ?>" placeholder="Name" id="name" name="name" class="form-control" data-minlength="3" data-pattern-error="Invalid name" data-error="Minimum of 3 characters" data-required-error="Name is Required" required autocomplete="off">
                                             <div class="help-block with-errors"></div>
                                         </div>
-                                    </dic>
-                                    <dic class="col-sm-6 col-md-6">
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
                                         <div class="form-group">
                                             <label for="roll_number" class="control-label">Roll Number</label>
                                             <input type="text" pattern="^[a-zA-Z0-9. ]+$" value="<?php if(!(empty($user))){echo($user->role_number);} ?>" placeholder="Roll number" id="roll_number" name="roll_number" class="form-control" data-pattern-error="Invalid roll number" data-required-error="Roll number is required" required autocomplete="off">
                                             <div class="help-block with-errors"></div>
                                         </div>
-                                    </dic>
+                                    </div>
                                     <div class="col-sm-6 col-md-6">
                                         <div class="form-group">
                                             <label for="class_id" class="control-label">Circle</label> 
@@ -78,6 +78,44 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <label for="user_pic" class="control-label">Profile Picture</label>
+                                            <input type="hidden" value="<?php if(!(empty($user))){if(trim($user->user_pic)!=''&&$user->user_pic!=null){echo('photos/staff/'.$user->user_pic.'-std.'.$user->extention);}else{echo('photos/default.jpg');}}else{echo('photos/default.jpg');} ?>" name="user_pic" id="user_pic">
+                                            <div class="row gutter-sm">
+                                                <div id="imageupdiv"></div>
+                                            </div>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <label for="parent_name" class="control-label">Parent Name</label>
+                                            <input type="text" pattern="^[a-zA-Z. ]+$" value="<?php if(!(empty($user))){echo($user->parent_name);} ?>" placeholder="Parent Name" id="parent_name" name="parent_name" class="form-control" data-minlength="3" data-pattern-error="Invalid parent name" data-error="Minimum of 3 characters" data-required-error="Parent name is required" required autocomplete="off">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <label for="parent_phone" class="control-label">Parent Phone</label> 
+                                            <input type="text" pattern="^[0-9+]+$" value="<?php if(!(empty($user))){echo($user->parent_phone);} ?>" placeholder="Parent Phone"  id="parent_phone" name="parent_phone" class="form-control" data-pattern-error="Invalid phone number format" autocomplete="off">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <label for="parent_email" class="control-label">Parent Email</label>
+                                            <input type="email" class="form-control" id="parent_email" name="parent_email" value="<?php if(!(empty($user))){echo($user->parent_email);} ?>" placeholder="Parent Email" data-error="Please enter a valid email address." required data-required-error="Email is required" autocomplete="off">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <label for="password" class="control-label">Password</label>
+                                            <input type="password" class="form-control" id="password" name="password" value="<?php if(!(empty($user))){echo($user->password);} ?>" placeholder="Password" required data-required-error="Password is required" autocomplete="off">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <button type="submit" class="btn btn-primary btn-block" id="submitBtn">Submit</button>
@@ -96,6 +134,25 @@
         <script src="<?=base_url()?>assets/js/spartan-multi-image-picker.js"></script>
         <script type="text/javascript">
 
+            $("#imageupdiv").spartanMultiImagePicker({
+                fieldName:'fileUpload',
+                maxCount:1,
+                rowHeight:'120px',
+                maxFileSize:5500000,
+                allowedExt:'jpg|jpeg|png',
+                dropFileLabel:   'Drop logo here',
+                groupClassName : 'col-md-4 col-sm-4 col-xs-6',
+                placeholderImage: {image:'<?=base_url();?>'+$('#user_pic').val() ,width: '60%'},
+                onRenderedPreview : function(index){
+                },
+
+                onExtensionErr : function(index, file){
+                    toastr["error"]('Please only input png or jpg type file');
+                },
+                onSizeErr : function(index, file){
+                    toastr["error"]('This file exceeds the max size(5MB)');
+                }
+            });
         </script>
     </body>
 </html>
